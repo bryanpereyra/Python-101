5e6200a7302d43239580747dec644077cfac8f2b
Bryan Pereyra <bryanqp0407@gmail.com>
2023-06-14 19:48:26 -0600

    Python cheatsheet

---
+++ cheatsheet.py
@@ -0,0 +1,128 @@
+## Sintaxis básica ##
+
+# Comentarios de una línea
+"""
+Comentarios multilínea
+"""
+
+# Definición de variables
+nombre_variable = valor
+
+# Impresión de valores
+print(valor)
+
+# Entrada de datos
+input("Mensaje de entrada: ")
+
+# Condicionales
+if condicion:
+    # Código si la condición es verdadera
+else:
+    # Código si la condición es falsa
+
+# Bucles
+for elemento in iterable:
+    # Código a repetir
+
+while condicion:
+    # Código a repetir
+
+# Funciones
+def nombre_funcion(parametros):
+    # Código de la función
+    return valor
+
+# Listas
+lista = [elemento1, elemento2, elemento3]
+
+# Acceso a elementos de una lista
+elemento = lista[indice]
+
+# Agregar elemento a una lista
+lista.append(elemento)
+
+# Diccionarios
+diccionario = {"clave1": valor1, "clave2": valor2}
+
+# Acceso a valores de un diccionario
+valor = diccionario["clave"]
+
+# Agregar un par clave-valor a un diccionario
+diccionario["clave"] = valor
+
+# Tuplas
+tupla = (elemento1, elemento2, elemento3)
+
+# Acceso a elementos de una tupla
+elemento = tupla[indice]
+
+# Conjuntos
+conjunto = {elemento1, elemento2, elemento3}
+
+# Operadores lógicos
+and, or, not
+
+# Operadores de comparación
+==, !=, <, >, <=, >=
+
+# Operadores aritméticos
++, -, *, /, %, **
+
+# Importar módulos
+import modulo
+
+# Importar una función específica de un módulo
+from modulo import funcion
+
+# Manejo de excepciones
+try:
+    # Código que puede lanzar una excepción
+except TipoDeExcepcion:
+    # Código a ejecutar si se captura la excepción
+finally:
+    # Código que se ejecuta siempre, haya excepción o no
+
+## Bibliotecas populares ##
+# Estas bibliotecas pueden requerir instalación previa
+
+# Matemáticas y cálculos científicos
+import numpy
+import scipy
+import pandas
+
+# Desarrollo web
+import flask
+import django
+
+# Análisis de datos
+import pandas
+import matplotlib
+import seaborn
+
+# Aprendizaje automático
+import scikit-learn
+import tensorflow
+import keras
+
+# Procesamiento de texto y lenguaje natural
+import nltk
+import spaCy
+
+# Automatización de pruebas
+import unittest
+import pytest
+import selenium
+
+# Manipulación de archivos y directorios
+import os
+import shutil
+import glob
+
+# Trabajo con bases de datos
+import sqlite3
+import pymysql
+import psycopg2
+
+# Fechas y tiempo
+import datetime
+import time
