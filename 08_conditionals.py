1d25ba5b251e1edea56b88381ae3dd6f3cff9ae1
Bryan Pereyra <bryanqp0407@gmail.com>
2023-06-21 10:31:55 -0600

    Conditionals

---
+++ 08_conditionals.py
@@ -0,0 +1,21 @@
+# Conditionals
+
+# Ejemplo de condicional en Python
+edad = 20
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+# Ejemplo de condicional con múltiples casos
+nota = 80
+
+if nota >= 90:
+    print("Tienes una calificación A")
+elif nota >= 80:
+    print("Tienes una calificación B")
+elif nota >= 70:
+    print("Tienes una calificación C")
+else:
+    print("Tienes una calificación inferior a C")
